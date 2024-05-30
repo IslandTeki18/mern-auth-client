da45f10208b419d8f7a3681f93c272ca5438dc1e
Landon McKell <lmckell@nerdunited.com>
2024-05-30 15:37:13 -0600

    chore: Add local storage check for user info on page load

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { createContext, useReducer } from "react";
+import { useEffect } from "react";
 
 export const AuthContext = createContext<any>(null);
 
@@ -19,6 +20,12 @@ export const AuthProvider = ({ children }: any) => {
     user: null
   });
 
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"))
+    if (userInfo) {
+      dispatch({ type: "LOGIN", payload: userInfo })
+    }
+  }, []);
 
   console.log("AuthContext state: ", state)
 
